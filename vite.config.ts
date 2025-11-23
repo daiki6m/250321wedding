4a66dca4f4b0fa6c19a3998c71be8b96bc16b36c
daiki6g <daiki6guitar@gmail.com>
2025-11-23 23:01:29 +0900

    Fix: Use conditional base path for dev and production

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/250321wedding/',
+    base: process.env.NODE_ENV === 'production' ? '/250321wedding/' : '/',
     plugins: [react()],
 })
